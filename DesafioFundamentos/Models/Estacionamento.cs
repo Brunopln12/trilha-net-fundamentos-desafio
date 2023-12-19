afff2c4f6798e4bc12c03c8d77660c7f9bbab9de
Brunopln12 <bruno_pinho_lobo@hotmail.com>
2023-12-19 20:57:55 -0300

    Desafio Finalizado

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -43,6 +43,7 @@ namespace DesafioFundamentos.Models
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
+                veiculos.Remove(placa);
                 
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
@@ -61,6 +62,9 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                foreach (string item in veiculos) { 
+                    Console.WriteLine(item);
+                }
             }
             else
             {
